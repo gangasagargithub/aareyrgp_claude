8bb91433b4534f13f7c57be428f427740d2a2980
gangasagargithub <gangasagar@prajapati.ind.in>
2026-07-24 11:40:31 +0530

    Change Brand Name

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -8,7 +8,7 @@ $initials = strtoupper(substr($_SESSION['first_name'] ?? '?', 0, 1) . substr($_S
 <aside class="sidebar">
   <div class="brand">
     <span class="brand-dot"></span>
-    <span class="brand-name">RBAC Console<small>aareyrgp_claude</small></span>
+    <span class="brand-name">WORKFLOW<small>aareyrgp_claude</small></span>
   </div>
 
   <div class="nav-group">
@@ -16,13 +16,11 @@ $initials = strtoupper(substr($_SESSION['first_name'] ?? '?', 0, 1) . substr($_S
     <a class="nav-link <?= $currentPage === 'dashboard.php' ? 'active' : '' ?>" href="dashboard.php">
       <span class="nav-icon">&#9635;</span> Dashboard
     </a>
-  </div>
- <div class="nav-group">
-        <div class="nav-label">Search</div>
-        <a class="nav-link <?= $currentPage === 'search.php' ? 'active' : '' ?>" href="search.php">
+    <a class="nav-link <?= $currentPage === 'search.php' ? 'active' : '' ?>" href="search.php">
       <span class="nav-icon">&#128269;</span> Search
     </a>
-      </div>
+  </div>
+
   <div class="nav-group">
     <div class="nav-label">Access Control</div>
     <a class="nav-link <?= $currentPage === 'users.php' ? 'active' : '' ?>" href="users.php">
